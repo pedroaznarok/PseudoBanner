072be0a431a9735634ce265c60df4f7e2ec94351
Pedro Aznar <pedroaznarrottweilas@gmail.com>
2022-06-09 13:23:20 -0300

    Pseudo Banner

---
+++ src/models/banner.ts
@@ -0,0 +1,11 @@
+
+export default class Plato{
+
+    id:number = 0;
+    urlImagenBanner:string = "";
+    textCaption:string = "";
+    texCaption:string = "";
+    descripcionImagen:string = "";
+    ordenSlider:Number = 0;
+
+}
